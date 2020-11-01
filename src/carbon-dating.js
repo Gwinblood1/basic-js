05bbcc63fe8eb5b9cbc812f680abb25d9f99ed24
Maksim Ivankou <fahrenheit9110@gmail.com>
2020-11-01 11:50:52 +0300

    init: start basic-js task

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -4,6 +4,28 @@ const MODERN_ACTIVITY= 15;
 const HALF_LIFE_PERIOD= 5730;
 
 module.exports = function dateSample(/* sampleActivity */) {
-  throw new CustomError('Not implemented');
+  if(typeof sampleActivity != 'string'){
+    return false;
+  }
+  let arr = sampleActivity.split('');
+  let havePoint = false;
+  for(let i = 0; i<arr.length; i++){
+    if(arr[i] == '.'){
+      if(havePoint == false){
+        havePoint = true;
+      }
+      else{
+        arr.splice(i,1);
+      }
+    }
+  }
+  sampleActivity = arr.join('');
+
+  if(isNaN(+ sampleActivity) || +sampleActivity > MODERN_ACTIVITY || +sampleActivity <= 0 ){
+    return false;
+  }
+  return Math.floor(HALF_LIFE_PERIOD / 0.693 * Math.log(sampleActivity/MODERN_ACTIVITY) * -1 )+1;
+
+
   // remove line with error and write your code here
 };
